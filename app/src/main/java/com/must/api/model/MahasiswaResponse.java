f6412cd585e1447c71f5d54e27553451608879c5
linerama4 <a.code.must@gmail.com>
2022-12-11 16:19:36 +0700

    api

---
+++ app/src/main/java/com/must/api/model/MahasiswaResponse.java
@@ -0,0 +1,16 @@
+package com.must.api.model;
+
+import java.util.List;
+
+public class MahasiswaResponse {
+    @SerializedName("data")
+    private List<Mahasiswa> data;
+    @SerializedName("status")
+    private boolean status;
+    public List<Mahasiswa> getData(){
+        return data;
+    }
+    public boolean isStatus() {
+        return status;
+    }
+}
